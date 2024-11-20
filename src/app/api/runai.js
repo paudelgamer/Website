f6be67d2a9a32fd0a5a4446c369ac2ce73671c4a
asur-zx <lamsal.aayush123@gmail.com>
2024-11-20 23:11:32 +0545

    feat: added ai

---
+++ src/app/api/runai.js
@@ -0,0 +1,22 @@
+
+"use server"; // Tells Next.js that this logic should run on the server
+
+import { exec } from "child_process";
+import path from "path";
+
+export async function runPythonScript() {
+  const pythonScriptPath = path.join(process.cwd(), "AI", "poly2.py");
+
+  // Run the Python script using exec
+  return new Promise((resolve, reject) => {
+    exec(`python3 ${pythonScriptPath}`, (error, stdout, stderr) => {
+      if (error) {
+        reject(`Error executing Python script: ${error.message}`);
+      }
+      if (stderr) {
+        reject(`Python script stderr: ${stderr}`);
+      }
+      resolve(stdout);
+    });
+  });
+}
